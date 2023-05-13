d1d6609184cdfa7f2dde3e0f9327735a412ff936
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-05-12 21:23:02 -0700

    dotTLD.sh [V1]
    dotTLD.sh [V1]

---
+++ Scripts/dotTLD/dotTLD.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#############
+# dotTLD.sh #
+######################################################################
+# - Scrubs all instances of dotdomain and replaces them with .domain #
+######################################################################
+# Source code
+break;
+# Coming soon
+break;
+# End of source code
+###
+# File info
+# File type: Shell source file (*.sh)
+# File version: 1 (2023, Friday, May 12th at 9:23 pm PST)
+# Line count (including blank lines and compiler line): 19
+###
+# End of script
